ba9cf310f214e9bd0cec348ea99cdc56af3cbf3a
Jonathan Sæthre Ege <1599278+yotasage@users.noreply.github.com>
2025-10-08 01:19:24 +0200

    ADDED: The follwoing have been added to Coordinate: __hash__, __eq__, __neg__, magnitude, mag, __abs__, __contains__, __len__, and translate CHANGED: Coordinate has manually been made immutable instead of using @dataclass(frozen=True) CHANGED: Coordinate has been made subclass-friendly

--- pade/layout/geometry.py
+++ pade/layout/geometry.py
@@ -1,12 +1,41 @@
 import numpy as np
-import copy
-from dataclasses import dataclass
-from typing import Iterable
+from typing import Iterable, Union
 
-@dataclass(frozen=True) # This makes the Coordiante immutable
 class Coordinate:
-    x: float
-    y: float
+    # Manual immutability
+    __slots__ = ("_x", "_y")  # optional, but makes instances smaller and faster
+
+    def __init__(self, *args, x=None, y=None):
+        if x is not None and y is not None:
+            _x, _y = float(x), float(y)
+        elif len(args) == 2:
+            _x, _y = float(args[0]), float(args[1])
+        elif len(args) == 1:
+            arg = args[0]
+            if isinstance(arg, Coordinate):
+                _x, _y = float(arg.x), float(arg.y)
+            elif hasattr(arg, "__getitem__") and len(arg) == 2:
+                _x, _y = float(arg[0]), float(arg[1])
+            else:
+                raise TypeError(f"Invalid coordinate initializer: {arg}")
+        else:
+            raise TypeError("Coordinate accepts (x, y), ((x, y)), [x, y], or another Coordinate")
+
+        # store as private attributes
+        object.__setattr__(self, "_x", _x)
+        object.__setattr__(self, "_y", _y)
+
+    @property
+    def x(self) -> float:
+        return self._x
+
+    @property
+    def y(self) -> float:
+        return self._y
+    
+    # This prevents the attributes of the Coordinate from being changed after the Coordinate has been created (immutability guard)
+    def __setattr__(self, name, value):
+        raise AttributeError(f"{self.__class__.__name__} is immutable")
 
     def to_skill(self, decimals=3):
         return f"{round(self.x, decimals)}:{round(self.y, decimals)}"
@@ -15,14 +44,20 @@ class Coordinate:
         return [np.round(self.x, decimals=decimals), np.round(self.y, decimals=decimals)]
 
     def __iter__(self):
-        yield self.x
-        yield self.y
+        return iter((self._x, self._y))
+
+    def __len__(self):
+        return 2
 
     def tuple(self):
         return (self.x, self.y)
 
     def __round__(self, ndigits=0):
-        return Coordinate((round(self[0], ndigits), round(self[1], ndigits)))
+        return self.__class__((round(self._x, ndigits), round(self._y, ndigits)))
+
+    def __contains__(self, value):
+        """Return True if value matches x or y component."""
+        return value == self._x or value == self._y
 
     def __getitem__(self, item):
         if item == 0:
@@ -30,46 +65,67 @@ class Coordinate:
         elif item == 1:
             return self.y
         else:
-            raise ValueError(f'Invalid index for coordinate {item}')
+            raise ValueError(f'{item} is an invalid index for Coordinate. The index must be 0 or 1.')
 
-    def __add__(self, other):
-        """
-        Assumes other is subscriptable
-        """
-        if other is None:
-            return self
-        try:
-            return Coordinate((self[0] + other[0], self[1] + other[1]))
-        except:
-            return Coordinate((self[0] + other, self[1] + other))
+    def __add__(self, other: Union['Coordinate', tuple, list, float, int]):
+        if isinstance(other, Coordinate):
+            return self.__class__(self._x + other._x, self._y + other._y)
+        elif hasattr(other, "__getitem__"):
+            return self.__class__(self._x + other[0], self._y + other[1])
+        else:
+            return self.__class__(self._x + other, self._y + other)
 
     def __sub__(self, other):
-        """
-        Assumes other is subscriptable
-        """
-        if other is None:
-            return self
-        try:
-            return Coordinate((self[0] - other[0], self[1] - other[1]))
-        except:
-            return Coordinate((self[0] - other, self[1] - other))
+        if isinstance(other, Coordinate):
+            return self.__class__(self._x - other._x, self._y - other._y)
+        elif hasattr(other, "__getitem__"):
+            return self.__class__(self._x - other[0], self._y - other[1])
+        else:
+            return self.__class__(self._x - other, self._y - other)
         
     def __truediv__ (self, value: float):
-        return Coordinate((self[0] / value, self[1] / value))
+        return self.__class__(self._x / value, self._y / value)
 
     def __mul__(self, scalar: float):
-        return Coordinate(self.x * scalar, self.y * scalar)
+        return self.__class__(self.x * scalar, self.y * scalar)
 
     __rmul__ = __mul__
 
+    def __abs__(self) -> float:
+        """Return the vector magnitude (Euclidean norm)."""
+        return (self._x ** 2 + self._y ** 2) ** 0.5
+
+    def mag(self) -> float:
+        """Alias for abs(): vector magnitude."""
+        return abs(self)
+
+    def magnitude(self) -> float:
+        """Alias for abs(): vector magnitude."""
+        return abs(self)
+
+    def __neg__(self):
+        """Return a new Coordinate with both components negated."""
+        return self.__class__(-self._x, -self._y)
+
+    def __eq__(self, other):
+        return isinstance(other, Coordinate) and self._x == other._x and self._y == other._y # polymorphic equality
+        # return type(other) is type(self) and self._x == other._x and self._y == other._y # subclass awareness
+
+    def __hash__(self):
+        return hash((self._x, self._y))
+
     def __str__(self) -> str:
-        return f'Coordinate({self[0]},{self[1]})'
+        return f'{self.__class__.__name__}({self._x}, {self._y})'
 
     def __repr__(self) -> str:
-        return f'Coordinate({self[0]},{self[1]})'
+        return f'{self.__class__.__name__}({self._x}, {self._y})'
     
-    def translate(self, dx=0, dy=0):
-        return Coordinate(self.x + dx, self.y + dy)
+    def __deepcopy__(self, memo):
+        # Since immutable, just return self
+        return self
+
+    def translate(self, dx=0.0, dy=0.0):
+        return self.__class__(self.x + dx, self.y + dy)
     
     @classmethod
     def midpoint(cls, a: 'Coordinate', b: 'Coordinate'):
@@ -83,7 +139,7 @@ class Coordinate:
         coords = list(coords) # supports any iterable
 
         total = sum(coords[1:], coords[0])
-        return total / len(coords)
+        return cls(total.x / len(coords), total.y / len(coords))
 
 class Vector:
     """
@@ -176,7 +232,6 @@ class Vector:
             return 3
         else:
             return 4
-
 
 class Line:
     """
